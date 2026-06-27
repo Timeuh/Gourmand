0e1a1e466e5328b8da90370ab175c89e768926cd
Timeuh <timeuuh@gmail.com>
2026-06-27 19:38:33 +0200

    ✨ create color update endpoint

---
+++ server/api/colors/[id].put.ts
@@ -0,0 +1,26 @@
+// update a color endpoint
+export default defineEventHandler(async (event) => {
+  try {
+    // get id from the request parameters
+    const id = Number(getRouterParam(event, "id"));
+
+    // read the request body and validate its data
+    const body = await readBody<ColorUpdate>(event);
+    const validatedBody: ColorUpdate =
+      await colorUpdateValidator.validate(body);
+
+    // update color data
+    const updatedColor: Color = await prisma.color.update({
+      where: {
+        id: id,
+      },
+      data: validatedBody,
+    });
+
+    // return the updated color
+    return sendJsonResponse<Color>(updatedColor, HTTP_OK);
+  } catch (error) {
+    // handle any errors that occur during the process
+    return sendErrorResponse(error);
+  }
+});
