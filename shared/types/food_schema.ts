07ff68765e24908ea2a7a91f23ff5df0053b5dfd
Timeuh <timeuuh@gmail.com>
2026-06-29 21:37:44 +0200

    🦺 create food schema for validation and types

---
+++ shared/types/food_schema.ts
@@ -0,0 +1,45 @@
+import vine from "@vinejs/vine";
+import type { Infer } from "@vinejs/vine/types";
+
+/* -------------------------------------------------------------------------- */
+/*                                  Schemas                                   */
+/* -------------------------------------------------------------------------- */
+const FOOD_SCHEMA = vine.object({
+  id: vine.number(),
+  name: vine.string(),
+  image: vine.string(),
+  plate_id: vine.number(),
+  preptime_id: vine.number(),
+});
+
+const FOOD_CREATE_SCHEMA = vine.object({
+  name: vine.string(),
+  image: vine.string(),
+  plate_id: vine.number(),
+  preptime_id: vine.number(),
+});
+
+const FOOD_UPDATE_SCHEMA = vine.object({
+  name: vine.string(),
+  image: vine.string(),
+  plate_id: vine.number(),
+  preptime_id: vine.number(),
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                   Types                                    */
+/* -------------------------------------------------------------------------- */
+export type Food = Infer<typeof FOOD_SCHEMA>;
+
+export type FoodCreation = Infer<typeof FOOD_CREATE_SCHEMA>;
+
+export type FoodUpdate = Infer<typeof FOOD_UPDATE_SCHEMA>;
+
+/* -------------------------------------------------------------------------- */
+/*                                 Validators                                 */
+/* -------------------------------------------------------------------------- */
+export const foodValidator = vine.create(FOOD_SCHEMA);
+
+export const foodCreateValidator = vine.create(FOOD_CREATE_SCHEMA);
+
+export const foodUpdateValidator = vine.create(FOOD_UPDATE_SCHEMA);
