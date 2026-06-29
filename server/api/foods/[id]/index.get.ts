8bb48d94b7f9367ce7dc28790ed72d7e7a0894a5
Timeuh <timeuuh@gmail.com>
2026-06-29 22:23:47 +0200

    ✨ display full food when asked

--- server/api/foods/[id]/index.get.ts
+++ server/api/foods/[id]/index.get.ts
@@ -1,14 +1,28 @@
 // get food by id endpoint
 export default defineEventHandler(async (event) => {
   try {
-    // get id from the request parameters
+    // get id and fullContent param from the request parameters
     const foodId = Number(getRouterParam(event, "id"));
+    const fullContent = getQuery(event).fullContent === "true";
 
     // get food from database by id
-    const food: Food | null = await prisma.food.findUnique({
+    const food: Food | FullFood | null = await prisma.food.findUnique({
       where: {
         id: foodId,
       },
+      include: fullContent
+        ? {
+            plate: true,
+            preptime: true,
+            foodIngredients: {
+              include: {
+                food_id: false,
+                ingredient_id: false,
+                ingredient: true,
+              },
+            },
+          }
+        : undefined,
     });
 
     // if food is not found, return an error
@@ -26,7 +40,7 @@ export default defineEventHandler(async (event) => {
     }
 
     // return the food
-    return sendJsonResponse<Food>(food, HTTP_OK);
+    return sendJsonResponse<Food | FullFood>(food, HTTP_OK);
   } catch (error) {
     // handle any errors that occur during the process
     return sendErrorResponse(error);
--- server/api/foods/index.get.ts
+++ server/api/foods/index.get.ts
@@ -1,11 +1,28 @@
 // get all foods endpoint
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   try {
+    // get fullContent param from the request parameters
+    const fullContent = getQuery(event).fullContent === "true";
+
     // get foods from database
-    const foods: Food[] = await prisma.food.findMany();
+    const foods: Food[] | FullFood[] = await prisma.food.findMany({
+      include: fullContent
+        ? {
+            plate: true,
+            preptime: true,
+            foodIngredients: {
+              include: {
+                food_id: false,
+                ingredient_id: false,
+                ingredient: true,
+              },
+            },
+          }
+        : undefined,
+    });
 
     // return the foods collection
-    return sendCollectionResponse<Food>(foods);
+    return sendCollectionResponse<Food | FullFood>(foods);
   } catch (error) {
     // handle any errors that occur during the process
     return sendErrorResponse(error);
