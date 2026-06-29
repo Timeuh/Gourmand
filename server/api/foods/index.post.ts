1c4e13476160cac69cefc8f946e2f056045a00a8
Timeuh <timeuuh@gmail.com>
2026-06-29 21:39:13 +0200

    ✨ create endpoint to create a food

---
+++ server/api/foods/index.post.ts
@@ -0,0 +1,24 @@
+// food creation endpoint
+export default defineEventHandler(async (event) => {
+  try {
+    // read the request body and validate its data
+    const body = await readBody<FoodCreation>(event);
+    const validatedBody = await foodCreateValidator.validate(body);
+
+    // create a new food in the database
+    const newFood: Food = await prisma.food.create({
+      data: {
+        name: validatedBody.name,
+        image: validatedBody.image,
+        plate_id: validatedBody.plate_id,
+        preptime_id: validatedBody.preptime_id,
+      },
+    });
+
+    // return the newly created food
+    return sendJsonResponse<Food>(newFood, HTTP_CREATED);
+  } catch (error) {
+    // handle any errors that occur during the process
+    return sendErrorResponse(error);
+  }
+});
