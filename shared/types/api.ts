cfcf88da69fcf125481915b4408d3d8d6ea0848c
Timeuh <timeuuh@gmail.com>
2026-07-01 19:24:13 +0200

    🏷️ add type for home data

--- shared/types/api.ts
+++ shared/types/api.ts
@@ -7,13 +7,25 @@ export type ApiError = {
   };
 };
 
+// collection returned by the api
 export type ApiCollection<T> = {
   type: string;
   count: number;
   items: T[];
 };
 
+// response returned by the api when uploading an image
 export type ImageUploadResponse = {
   filename: string;
   url: string;
+};
+
+// data returned by the api for the home page
+export type HomeData = {
+  foodsOfPreviousMonth: number;
+  foodsOfCurrentMonth: number;
+  favoriteFoods: Food[];
+  foodsOfThisWeek: FullCalendar[];
+  oldestFoods: OldestFood[];
+  mostEatenFoods: MostEatenFood[];
 };
