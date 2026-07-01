dd4479840ed367390be61d4fb702f5413180e0d4
Timeuh <timeuuh@gmail.com>
2026-07-01 17:54:36 +0200

    🩹 fix cases where userId is specified but not a valid number

--- server/api/calendars/index.get.ts
+++ server/api/calendars/index.get.ts
@@ -4,7 +4,22 @@ export default defineEventHandler(async (event) => {
     // get fullContent param from the request parameters
     const fullContent = getQuery(event).fullContent === "true";
     // get calendars from a specific user
-    const userId = Number(getQuery(event).userId);
+    const userId = getQuery(event).userId;
+
+    // if userId is specified but invalid, return a bad request error
+    if (userId !== undefined && isNaN(Number(userId))) {
+      return sendJsonResponse<ApiError>(
+        {
+          error: {
+            code: HTTP_BAD_REQUEST,
+            message: MSG_BAD_REQUEST,
+            details:
+              "Invalid userId parameter. Please provide a valid number for the userId query parameter.",
+          },
+        },
+        HTTP_BAD_REQUEST,
+      );
+    }
 
     // get calendars from database
     const calendars: Calendar[] | FullCalendar[] =
@@ -12,7 +27,7 @@ export default defineEventHandler(async (event) => {
         where: userId
           ? {
               food: {
-                user_id: userId,
+                user_id: Number(userId),
               },
             }
           : undefined,
--- server/api/foods/index.get.ts
+++ server/api/foods/index.get.ts
@@ -4,13 +4,28 @@ export default defineEventHandler(async (event) => {
     // get fullContent param from the request parameters
     const fullContent = getQuery(event).fullContent === "true";
     // get calendars from a specific user
-    const userId = Number(getQuery(event).userId);
+    const userId = getQuery(event).userId;
+
+    // if userId is specified but invalid, return a bad request error
+    if (userId !== undefined && isNaN(Number(userId))) {
+      return sendJsonResponse<ApiError>(
+        {
+          error: {
+            code: HTTP_BAD_REQUEST,
+            message: MSG_BAD_REQUEST,
+            details:
+              "Invalid userId parameter. Please provide a valid number for the userId query parameter.",
+          },
+        },
+        HTTP_BAD_REQUEST,
+      );
+    }
 
     // get foods from database
     const foods: Food[] | FullFood[] = await prisma.food.findMany({
       where: userId
         ? {
-            user_id: userId,
+            user_id: Number(userId),
           }
         : undefined,
       include: fullContent
