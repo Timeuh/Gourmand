ba2c65af1ac2ca5e8e6446668171ffc61d34e34e
Timeuh <timeuuh@gmail.com>
2026-07-08 14:22:31 +0200

    ✨ create fake data for default foods

---
+++ app/utils/default_foods.ts
@@ -0,0 +1,37 @@
+// default foods to use in the app when no data is available
+const defaultFoods: Food[] = [
+  {
+    image: "/assets/default_food.png",
+    id: 1,
+    name: "default_food_1",
+    plate_id: 1,
+    preptime_id: 1,
+    user_id: 1,
+  },
+  {
+    image: "/assets/default_food.png",
+    id: 2,
+    name: "default_food_2",
+    plate_id: 2,
+    preptime_id: 2,
+    user_id: 2,
+  },
+  {
+    image: "/assets/default_food.png",
+    id: 3,
+    name: "default_food_3",
+    plate_id: 3,
+    preptime_id: 3,
+    user_id: 3,
+  },
+  {
+    image: "/assets/default_food.png",
+    id: 4,
+    name: "default_food_4",
+    plate_id: 4,
+    preptime_id: 4,
+    user_id: 4,
+  },
+];
+
+export default defaultFoods;
