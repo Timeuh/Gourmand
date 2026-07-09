6316b17670a798e6da28d9b1e99938c64b92e7e3
Timeuh <timeuuh@gmail.com>
2026-07-09 16:20:03 +0200

    ✨ create component to display most eaten foods of this month

---
+++ app/components/MonthMostEaten.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+// define props types
+interface Props {
+  foods: MostEatenFood[] | undefined;
+}
+
+// register props
+const props = defineProps<Props>();
+
+// return foods from database or default foods if not available
+const foodsToDisplay: ComputedRef<MostEatenFood[]> = computed(() => {
+  return props.foods && props.foods.length > 0
+    ? props.foods
+    : mapDefaultFoods<MostEatenFood>((food: Food) => {
+        return {
+          food,
+          count: 1,
+        };
+      });
+});
+
+// calculate the percentage reprensented by current count in comparison with the most eaten food of the month
+function calculatePercentage(count: number) {
+  return Math.round((count / (foodsToDisplay.value[0]?.count || 1)) * 100);
+}
+</script>
+
+<template>
+  <div class="space-y-2 w-full">
+    <div class="flex flex-row justify-between items-center w-full">
+      <h2 class="font-bold text-secondary-900 text-lg">
+        📊 Les plus mangés ce mois
+      </h2>
+    </div>
+    <div
+      class="flex flex-col items-center bg-background-900 shadow-[0_1px_2px_0] shadow-secondary-900/50 p-4 rounded-xl w-full"
+    >
+      <div
+        v-for="food in foodsToDisplay"
+        class="flex flex-row justify-between items-center space-y-4 w-full"
+      >
+        <NuxtImg
+          :src="food.food?.image"
+          :alt="food.food?.name"
+          class="rounded-xl size-20 object-cover"
+        />
+        <div class="space-y-2 min-w-[55%]">
+          <h3 class="font-bold text-secondary-500 text-sm">
+            {{ food.food?.name }}
+          </h3>
+          <ProgressBar
+            height="h-3"
+            :percentage="calculatePercentage(food.count)"
+          />
+        </div>
+        <h4>
+          <span class="font-bold text-secondary-900">{{ food.count }}</span>
+          fois
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>
