dcccc812c94b8168468f8ab547d4aedcbdf38a6f
Timeuh <timeuuh@gmail.com>
2026-07-13 22:42:53 +0200

    ✨ handle fullContent parameter to display ingredient category as well

--- server/api/ingredients/index.get.ts
+++ server/api/ingredients/index.get.ts
@@ -1,11 +1,21 @@
 // get all ingredients endpoint
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   try {
+    // get fullContent param from the request parameters
+    const fullContent = getQuery(event).fullContent === "true";
+
     // get ingredients from database
-    const ingredients: Ingredient[] = await prisma.ingredient.findMany();
+    const ingredients: Ingredient[] | FullIngredient[] =
+      await prisma.ingredient.findMany({
+        include: fullContent
+          ? {
+              category: true,
+            }
+          : undefined,
+      });
 
     // return the ingredients collection
-    return sendCollectionResponse<Ingredient>(ingredients);
+    return sendCollectionResponse<Ingredient | FullIngredient>(ingredients);
   } catch (error) {
     // handle any errors that occur during the process
     return sendErrorResponse(error);
