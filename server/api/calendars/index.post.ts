e60386d431bfc2a23c472fc6537f5a9f4a8d37e0
Timeuh <timeuuh@gmail.com>
2026-07-17 14:37:24 +0200

    ♻️ remove day food limit

--- server/api/calendars/index.post.ts
+++ server/api/calendars/index.post.ts
@@ -5,30 +5,6 @@ export default defineEventHandler(async (event) => {
     const body = await readBody<CalendarCreation>(event);
     const validatedBody = await calendarCreateValidator.validate(body);
 
-    // count number of meals for the given user and date
-    const totalMeals = await prisma.calendar.count({
-      where: {
-        date: validatedBody.date,
-        food: {
-          user_id: validatedBody.user_id,
-        },
-      },
-    });
-
-    // meals cannot exceed 2 for a given user and date, return an error if the limit is exceeded
-    if (totalMeals >= 2) {
-      return sendJsonResponse<ApiError>(
-        {
-          error: {
-            code: HTTP_BAD_REQUEST,
-            message: MSG_MEAL_LIMIT_EXCEEDED,
-            details: `Meals for user with id ${validatedBody.user_id} on date ${validatedBody.date.toISOString()} are at the maximum limit of 2`,
-          },
-        },
-        HTTP_BAD_REQUEST,
-      );
-    }
-
     // create a new calendar in the database
     const newCalendar: Calendar = await prisma.calendar.create({
       data: {
