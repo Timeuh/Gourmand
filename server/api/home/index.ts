c78cc5bad245d34a74f5a427ec938a35e7490ec6
Timeuh <timeuuh@gmail.com>
2026-07-20 17:04:06 +0200

    🩹  fix last week start being 7 days from today instead of six, making a 8 days week

--- server/api/home/index.ts
+++ server/api/home/index.ts
@@ -36,7 +36,7 @@ export default defineEventHandler(async (event) => {
     const lastWeekStart = new Date(
       today.getFullYear(),
       today.getMonth(),
-      today.getDate() - 7,
+      today.getDate() - 6,
     );
     const previousMonthStart = new Date(
       today.getFullYear(),
