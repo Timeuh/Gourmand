ca98f2e99bb7336c51ab255dec86ca53abbadb9f
Timeuh <timeuuh@gmail.com>
2026-07-23 12:01:18 +0200

    ♻️ change add recipe button text

--- app/components/button/Add.vue
+++ app/components/button/Add.vue
@@ -3,6 +3,6 @@
     class="flex flex-row justify-center items-center space-x-2 bg-primary-900 hover:bg-secondary-900 shadow-[0_1px_2px_0] shadow-secondary-900/50 rounded-lg w-1/2 h-10 text-background-900 xl:text-lg transition-colors duration-300 ease-in-out cursor-pointer"
   >
     <IconPot class="size-5 xl:size-6" />
-    <h3>Ajouter un plat</h3>
+    <h3>Ajouter une recette</h3>
   </button>
 </template>
