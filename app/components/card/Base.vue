37a3be955a7856aae6f2a12b236845c4c166ac2d
Timeuh <timeuuh@gmail.com>
2026-07-24 14:24:29 +0200

    ✨ destructure card into composable cards

---
+++ app/components/card/Base.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+// define props types
+interface Props {
+  food: Food | undefined;
+  class: string;
+}
+
+// register props
+const props = defineProps<Props>();
+
+// in case an error happens
+const { cardError } = useEatFood();
+</script>
+
+<template>
+  <div
+    class="relative shadow-[0_2px_4px_0] shadow-secondary-900/50 rounded-xl overflow-hidden"
+    :class="props.class"
+  >
+    <h3 class="top-2 absolute w-full text-red-700 text-center">
+      {{ cardError }}
+    </h3>
+    <NuxtImg
+      :src="props.food?.image"
+      :alt="props.food?.name"
+      class="w-full h-full object-cover"
+    />
+    <slot name="overlay" />
+  </div>
+</template>
---
+++ app/components/card/LastEaten.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// define props types
+interface Props {
+  cardFood: OldestFood;
+  class: string;
+}
+
+// register props
+const props = defineProps<Props>();
+
+// get the function to eat a food
+const { eatFood } = useEatFood();
+
+// test if the food has been eaten
+const lastEaten = computed(() =>
+  props.cardFood.lastEaten != null
+    ? daysFromToday(new Date(props.cardFood.lastEaten || ""))
+    : "Pas encore mangé",
+);
+</script>
+
+<template>
+  <CardBase :class="props.class" :food="props.cardFood.food">
+    <template #overlay>
+      <div
+        class="bottom-0 absolute flex flex-row justify-between items-center bg-linear-to-t from-black via-60% via-black/80 to-black/20 backdrop-blur-xs p-2 w-full"
+      >
+        <div class="space-y-1 w-full">
+          <h3 class="text-background-900 text-sm text-balance">
+            {{ props.cardFood.food?.name }}
+          </h3>
+          <h4 class="font-bold text-secondary-100 text-xs">
+            {{ lastEaten }}
+          </h4>
+        </div>
+        <button
+          @click="eatFood(props.cardFood.food?.id)"
+          class="flex flex-col justify-center items-center bg-background-900 rounded-full size-7 cursor-pointer shrink-0"
+        >
+          <IconPlus class="size-6 text-primary-900" />
+        </button>
+      </div>
+    </template>
+  </CardBase>
+</template>
