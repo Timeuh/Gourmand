d50e3569a07519369c8602bb1efcfd5703784a50
Timeuh <timeuuh@gmail.com>
2026-07-24 14:40:33 +0200

    ✨ create card placeholder composable

---
+++ app/components/card/PlaceHolder.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// define props types
+interface Props {
+  class: string;
+  message: string;
+}
+
+// register props
+const props = defineProps<Props>();
+
+// create fake food for base card
+const fakeFood: Food = {
+  id: -1,
+  user_id: -1,
+  preptime_id: -1,
+  name: "",
+  image: "/assets/default_food.png",
+  plates: -1,
+};
+</script>
+
+<template>
+  <CardBase :class="props.class" :food="fakeFood">
+    <template #overlay>
+      <div
+        class="bottom-0 absolute flex flex-row justify-between items-center bg-background-900 p-2 w-full text-secondary-900"
+      >
+        <div class="space-y-1 w-full">
+          <h3 class="text-sm text-balance">
+            {{ props.message }}
+          </h3>
+        </div>
+      </div>
+    </template>
+  </CardBase>
+</template>
