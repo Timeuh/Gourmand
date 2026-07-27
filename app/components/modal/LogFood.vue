4696b14aaa657d027419777f4104cd28de5ec9d5
Timeuh <timeuuh@gmail.com>
2026-07-27 14:54:34 +0200

    💄 adapt log modal to desktop

--- app/components/modal/LogFood.vue
+++ app/components/modal/LogFood.vue
@@ -30,7 +30,9 @@ function clearSearch() {
     :class="showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'"
     class="z-20 fixed inset-0 flex flex-col justify-center items-center bg-black/50 backdrop-blur-xs w-full h-screen transition duration-500 ease-in-out"
   >
-    <div class="space-y-3 bg-background-500 p-4 rounded-xl w-4/5 h-4/5">
+    <div
+      class="space-y-3 bg-background-500 p-4 rounded-xl w-4/5 xl:w-1/3 h-4/5"
+    >
       <div class="flex flex-row justify-between items-center w-full">
         <h1 class="font-bold text-primary-900 text-xl">Log un plat</h1>
         <button @click="closeModal" class="cursor-pointer">
@@ -40,21 +42,25 @@ function clearSearch() {
       <div
         class="flex flex-row justify-between items-center bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 rounded-md focus-within:outline focus-within:outline-primary-900 w-full"
       >
-        <div class="flex flex-row items-center space-x-1">
+        <div class="flex flex-row items-center space-x-1 xl:w-full">
           <IconSearch class="size-5 text-secondary-500" />
           <input
             type="text"
             v-model="search"
             placeholder="Lasagnes"
-            class="outline-none text-secondary-900 placeholder:text-secondary-500"
+            class="outline-none xl:w-full text-secondary-900 placeholder:text-secondary-500"
           />
         </div>
-        <button v-if="search !== ''" @click="clearSearch">
+        <button
+          v-if="search !== ''"
+          @click="clearSearch"
+          class="cursor-pointer"
+        >
           <IconCross class="size-4 text-secondary-900" />
         </button>
       </div>
       <div
-        class="content-start gap-3 grid grid-cols-2 w-full h-[86%] overflow-auto"
+        class="content-start gap-3 grid grid-cols-2 xl:grid-cols-3 w-full h-[86%] overflow-auto"
       >
         <CardLastEaten
           v-for="food in foodsToDisplay"
