db1d1cd99eaff8a19cac5935477aa97b7ca0baa2
Timeuh <timeuuh@gmail.com>
2026-07-27 12:00:04 +0200

    ✨ add lastEaten param to get last time eaten for each food

--- server/api/foods/index.get.ts
+++ server/api/foods/index.get.ts
@@ -3,6 +3,8 @@ export default defineEventHandler(async (event) => {
   try {
     // get fullContent param from the request parameters
     const fullContent = getQuery(event).fullContent === "true";
+    // get fullContent param from the request parameters
+    const lastEaten = getQuery(event).lastEaten === "true";
     // get calendars from a specific user
     const userId = getQuery(event).userId;
 
@@ -28,30 +30,55 @@ export default defineEventHandler(async (event) => {
     const idToCheck = user?.id || Number(userId);
 
     // get foods from database
-    const foods: Food[] | FullFood[] = await prisma.food.findMany({
+    const foods = await prisma.food.findMany({
       where: idToCheck
         ? {
             user_id: idToCheck,
           }
         : undefined,
-      include: fullContent
-        ? {
-            preptime: true,
-            user: true,
-            foodIngredients: {
-              include: {
-                food_id: false,
-                ingredient_id: false,
-                ingredient: {
-                  include: {
-                    category: true,
-                  },
+      include: {
+        ...(fullContent && {
+          preptime: true,
+          user: true,
+          foodIngredients: {
+            include: {
+              ingredient: {
+                include: {
+                  category: true,
                 },
               },
             },
-          }
-        : undefined,
+          },
+        }),
+        ...(lastEaten && {
+          calendars: {
+            orderBy: {
+              date: "desc",
+            },
+            take: 1,
+            select: {
+              date: true,
+            },
+          },
+        }),
+      },
     });
+
+    // if the user wants last eaten date for each food
+    if (lastEaten) {
+      // format foods to match the OldestFood type
+      const foodsToReturn: OldestFood[] = foods.map(
+        ({ calendars, ...food }) => {
+          return {
+            food,
+            lastEaten: calendars[0]?.date ?? null,
+          } as OldestFood;
+        },
+      );
+
+      // return the formated foods
+      return sendCollectionResponse<OldestFood>(foodsToReturn);
+    }
 
     // return the foods collection
     return sendCollectionResponse<Food | FullFood>(foods);
