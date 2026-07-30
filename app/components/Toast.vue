6659f7396b31b7dc29a701dbd8620d8c309e6264
Timeuh <timeuuh@gmail.com>
2026-07-30 13:21:39 +0200

    💄 adapt toast to desktop format

--- app/components/Toast.vue
+++ app/components/Toast.vue
@@ -5,7 +5,7 @@ const { showToast, toastContent } = useToast();
 <template>
   <div
     :class="showToast ? 'translate-x-0' : 'translate-x-[120%]'"
-    class="right-2 bottom-[12vh] z-30 fixed flex flex-row items-center space-x-2 bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 rounded-md h-10 text-green-600 text-xl transition duration-300 ease-in-out"
+    class="right-2 bottom-[12vh] xl:bottom-[2vh] z-30 fixed flex flex-row items-center space-x-2 bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 rounded-md h-10 text-green-600 text-xl transition duration-300 ease-in-out"
   >
     <IconSuccess class="size-5" />
     <h2>{{ toastContent }}</h2>
