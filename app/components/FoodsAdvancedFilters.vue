d620110c6cf84eda06a5b54e019b149941492ab8
Timeuh <timeuuh@gmail.com>
2026-07-31 18:08:53 +0200

    ✨ create component to display the filters possibilities

---
+++ app/components/FoodsAdvancedFilters.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 rounded-md w-full h-[60vh] overflow-auto text-secondary-900"
+  >
+    <FormIngredients ref-key="FoodsIngredientsFilter" prefix="filter" />
+  </div>
+</template>
