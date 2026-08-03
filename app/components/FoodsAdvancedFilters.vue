414d3993d75f2e3aea4ab77718a5e111a130e2c7
Timeuh <timeuuh@gmail.com>
2026-08-03 16:30:46 +0200

    🚧 display food plates filter

--- app/components/FoodsAdvancedFilters.vue
+++ app/components/FoodsAdvancedFilters.vue
@@ -5,6 +5,7 @@
     class="space-y-4 bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/50 p-3 rounded-md w-full h-[60vh] overflow-auto text-secondary-900"
   >
     <FormPreptime ref-key="FoodsPreptimeFilter" prefix="filter" />
+    <FormPlates ref-key="FoodsPlatesFilter" prefix="filter" />
     <FormIngredients ref-key="FoodsIngredientsFilter" prefix="filter" />
   </div>
 </template>
