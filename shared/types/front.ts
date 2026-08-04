30fd7196fee56662ca745e4da1117d5a94e6832b
Timeuh <timeuuh@gmail.com>
2026-08-04 16:13:58 +0200

    🏷️ add type for a calendar day

--- shared/types/front.ts
+++ shared/types/front.ts
@@ -3,3 +3,10 @@ export type ProgressBarStop = {
   stop: number;
   justify: string;
 };
+
+// a calendar day entry
+export type CalendarDay = {
+  day: number;
+  month: number;
+  year: number;
+};
