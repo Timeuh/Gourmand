a279df501fdcc1ee272d6e8402b46e347922b3de
Timeuh <timeuuh@gmail.com>
2026-08-04 13:57:54 +0200

    ✨ display title and date

--- app/pages/calendar.vue
+++ app/pages/calendar.vue
@@ -9,6 +9,16 @@ definePageMeta({
 
 <template>
   <div class="bg-background-500 p-8 w-full xl:w-5/6 h-screen">
-    <h1>Calendrier</h1>
+    <section
+      id="first-row"
+      class="xl:flex flex-row justify-between space-y-6 xl:space-y-0 w-full"
+    >
+      <div>
+        <h1 class="font-bold text-secondary-900 text-xl">🗓️ Calendrier</h1>
+        <h2 class="text-md text-secondary-500">
+          {{ formatDate(new Date()) }}
+        </h2>
+      </div>
+    </section>
   </div>
 </template>
