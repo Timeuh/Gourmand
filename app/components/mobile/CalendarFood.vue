877ea8b5dd0779e78ec993530f33fe5008edcc73
Timeuh <timeuuh@gmail.com>
2026-08-05 17:26:00 +0200

    ✨ display date and foods of the day

--- app/components/mobile/CalendarFood.vue
+++ app/components/mobile/CalendarFood.vue
@@ -2,19 +2,35 @@
 // get calendar utils from composable
 const { selectedDate, showFoods, hideFoodsPanel } = useCalendarUtils();
 
+// format selected date as "YYYY-MM-DD"
+const formatedSelectedDate = computed(
+  () =>
+    `${selectedDate.value.year}-${selectedDate.value.month}-${selectedDate.value.day}`,
+);
+
 // fetch calendar data for the selected date
-const { data } = useFetch<ApiCollection<Calendar>>("/api/calendars", {
+const { data } = useFetch<ApiCollection<FullCalendar>>("/api/calendars", {
   key: "CalendarDay",
   query: computed(() => ({
-    date: `${selectedDate.value.year}-${selectedDate.value.month}-${selectedDate.value.day}`,
+    date: formatedSelectedDate.value,
+    fullContent: true,
   })),
+});
+
+// selected date, formated with desired format
+const formatedDate = computed(() => {
+  return new Date(formatedSelectedDate.value).toLocaleDateString("fr-FR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 });
 </script>
 
 <template>
   <div
-    :class="showFoods ? 'translate-y-0' : 'translate-y-[80%]'"
-    class="bottom-20 left-0 fixed bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/25 rounded-md w-full h-40 transition duration-300 ease-in-out"
+    :class="showFoods ? 'translate-y-0' : 'translate-y-[90%]'"
+    class="bottom-20 left-0 fixed bg-background-900 shadow-[0_0_2px_0] shadow-secondary-900/25 rounded-md w-full h-fit transition duration-300 ease-in-out"
   >
     <div class="flex flex-row justify-center items-center pt-2 w-full">
       <button
@@ -22,10 +38,35 @@ const { data } = useFetch<ApiCollection<Calendar>>("/api/calendars", {
         @click="hideFoodsPanel"
       />
     </div>
-    <div class="p-2">
-      <h3>
-        {{ data?.items }}
-      </h3>
+    <div class="pb-3">
+      <div class="p-3">
+        <div class="bg-background-500 p-1 rounded-md w-full text-secondary-900">
+          <h3 class="text-lg text-center">{{ formatedDate }}</h3>
+        </div>
+      </div>
+      <div class="space-y-3 p-3 pt-2 w-full h-[15vh] overflow-auto">
+        <div
+          v-for="calendar in data?.items"
+          :key="calendar.id"
+          class="flex flex-row items-center space-x-4 bg-background-900 shadow-[0_0_3px_0] shadow-secondary-900/50 p-2 rounded-md w-full"
+        >
+          <NuxtImg
+            :src="calendar.food.image"
+            :alt="calendar.food.name"
+            class="rounded-md size-16 object-cover"
+          />
+          <h3 class="font-bold text-secondary-900 text-xl">
+            {{ calendar.food.name }}
+          </h3>
+        </div>
+      </div>
+      <div class="p-3">
+        <button
+          class="flex flex-row justify-center items-center space-x-4 bg-background-900 p-2 border-2 border-secondary-900/50 border-dotted rounded-md w-full h-18"
+        >
+          <IconPlus class="size-7 text-secondary-900" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
