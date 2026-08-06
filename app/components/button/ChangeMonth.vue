1a9329b1ae0b7485680d4f10c825c9fe5a4da157
Timeuh <timeuuh@gmail.com>
2026-08-06 13:48:10 +0200

    💄 adapt button size on desktop

--- app/components/button/ChangeMonth.vue
+++ app/components/button/ChangeMonth.vue
@@ -13,7 +13,7 @@ const monthName = computed(() => {
 
 <template>
   <div
-    class="relative flex flex-row justify-center items-center space-x-2 bg-background-900 shadow-[0_0_1px_0] shadow-secondary-900/50 p-2 rounded-md min-w-[45vw] text-secondary-900"
+    class="relative flex flex-row justify-center items-center space-x-2 bg-background-900 shadow-[0_0_1px_0] shadow-secondary-900/50 p-2 rounded-md min-w-[45vw] xl:min-w-[10vw] text-secondary-900"
   >
     <button @click="changeMonth(-1)" class="left-2 absolute">
       <IconChevronLeft class="size-5" />
