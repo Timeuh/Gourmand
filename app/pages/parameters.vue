fe3c96b9eba3f9e1e31bb0595d334650992be7a3
Timeuh <timeuuh@gmail.com>
2026-08-12 22:55:21 +0200

    ✨ display title and current date

--- app/pages/parameters.vue
+++ app/pages/parameters.vue
@@ -7,16 +7,21 @@ const { user, loggedIn, clear } = useUserSession();
 </script>
 
 <template>
-  <div class="bg-background-500 p-8 w-full xl:w-5/6 h-screen">
-    <h1>Paramètres</h1>
-
+  <div
+    class="space-y-6 bg-background-500 p-8 w-full xl:w-5/6 h-screen select-none"
+  >
+    <section id="first-row">
+      <h1 class="font-bold text-secondary-900 text-xl">⚙️ Paramètres</h1>
+      <h2 class="text-md text-secondary-500">
+        {{ formatDate(new Date()) }}
+      </h2>
+    </section>
     <div v-if="loggedIn">
       <p>Bienvenue, {{ user?.name }} !</p>
       <button @click="clear" class="bg-red-500 px-4 py-2 rounded text-white">
         Se déconnecter
       </button>
     </div>
-
     <div v-else>
       <a
         href="/api/auth/google"
