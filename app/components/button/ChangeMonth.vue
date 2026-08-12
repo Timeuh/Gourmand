f84297722e57c4f354063e8180f1ab657114b3e7
Timeuh <timeuuh@gmail.com>
2026-08-12 22:47:52 +0200

    💄 make buttons cursor pointer on hover

--- app/components/button/ChangeMonth.vue
+++ app/components/button/ChangeMonth.vue
@@ -15,11 +15,17 @@ const monthName = computed(() => {
   <div
     class="relative flex flex-row justify-center items-center bg-background-900 shadow-[0_0_1px_0] shadow-secondary-900/50 p-2 rounded-md min-w-[42vw] xl:min-w-[10vw] text-secondary-900"
   >
-    <button @click="changeMonth(-1)" class="left-1 xl:left-2 absolute">
+    <button
+      @click="changeMonth(-1)"
+      class="left-1 xl:left-2 absolute cursor-pointer"
+    >
       <IconChevronLeft class="size-5" />
     </button>
     <h2>{{ monthName }}</h2>
-    <button @click="changeMonth(1)" class="right-1 xl:right-2 absolute">
+    <button
+      @click="changeMonth(1)"
+      class="right-1 xl:right-2 absolute cursor-pointer"
+    >
       <IconChevronRight class="size-5" />
     </button>
   </div>
