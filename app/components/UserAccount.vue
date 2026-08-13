b805c0e93d19404191c3ec4f838b24f3e95ef653
Timeuh <timeuuh@gmail.com>
2026-08-13 20:09:46 +0200

    💄 style user account section when logged in

--- app/components/UserAccount.vue
+++ app/components/UserAccount.vue
@@ -3,19 +3,37 @@ const { user, loggedIn, clear } = useUserSession();
 </script>
 
 <template>
-  <section>
-    <h2>Compte</h2>
-    <section v-if="loggedIn">
-      <div>
-        <NuxtImg :src="user?.picture" alt="image de profil" />
+  <section
+    class="flex flex-col items-center space-y-4 bg-background-900 shadow-[0_1px_2px_0] shadow-secondary-900/50 p-8 rounded-xl w-full text-secondary-900"
+  >
+    <h2 class="font-bold text-2xl">Compte</h2>
+    <section v-if="loggedIn" class="space-y-8 w-full">
+      <div class="flex flex-row items-center space-x-4 w-full">
+        <NuxtImg
+          :src="user?.picture"
+          alt="image de profil"
+          class="shadow-[0_1px_2px_0] shadow-secondary-900/50 rounded-full size-24"
+        />
         <div>
-          <h3>{{ user?.name }}</h3>
-          <h4>{{ user?.email }}</h4>
+          <h3 class="font-bold">{{ user?.name }}</h3>
+          <h4 class="text-md text-secondary-500">{{ user?.email }}</h4>
         </div>
       </div>
-      <div>
-        <NuxtLink to="/api/auth/google">Changer de compte</NuxtLink>
-        <button @click="clear">Se déconnecter</button>
+      <div class="flex flex-col items-center space-y-4 w-full text-center">
+        <a
+          href="/api/auth/google"
+          class="flex flex-row justify-center items-center space-x-2 bg-background-500 shadow-[0_1px_2px_0] shadow-secondary-900/50 rounded-xl w-full h-12"
+        >
+          <IconSwitchAccount class="size-6" />
+          <h4 class="text-lg">Changer de compte</h4>
+        </a>
+        <button
+          @click="clear"
+          class="flex flex-row justify-center items-center space-x-2 bg-primary-900 shadow-[0_1px_2px_0] shadow-secondary-900/50 rounded-xl w-full h-12 text-background-900"
+        >
+          <IconDisconnect class="size-6" />
+          <h4 class="text-lg">Se déconnecter</h4>
+        </button>
       </div>
     </section>
     <section v-else>
@@ -27,7 +45,7 @@ const { user, loggedIn, clear } = useUserSession();
         </div>
       </div>
       <div>
-        <NuxtLink to="/api/auth/google">Se connecter</NuxtLink>
+        <a href="/api/auth/google">Se connecter</a>
       </div>
     </section>
   </section>
