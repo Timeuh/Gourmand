af9539aafa1d0069014edaca48dbba7505079000
Timeuh <timeuuh@gmail.com>
2026-08-14 17:54:47 +0200

    💄 adapt to desktop display

--- app/components/user/Theme.vue
+++ app/components/user/Theme.vue
@@ -15,34 +15,38 @@ const { loggedIn } = useUserSession();
       <h2 class="font-bold text-2xl text-center">Thème de l'application</h2>
       <h3 class="text-md text-secondary-500">Choisissez un thème</h3>
     </div>
-    <label
-      v-for="theme in data?.items"
-      :key="theme.id"
-      class="group space-y-2 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 border border-transparent has-checked:border-primary-900 rounded-xl w-full"
+    <div
+      class="gap-3 grid grid-cols-1 xl:grid-cols-2 p-1 w-full xl:h-[21vh] overflow-scroll"
     >
-      <div class="flex flex-row justify-between items-center w-full">
-        <h3 class="font-bold">{{ theme.name }}</h3>
-        <input
-          v-model="currentTheme"
-          type="radio"
-          :value="theme.id"
-          name="theme"
-          class="size-5 accent-primary-900"
-        />
-      </div>
-      <div class="gap-2 grid grid-cols-4 w-full">
-        <div
-          v-for="color in theme.themeColors"
-          :key="color.color.code"
-          class="w-full h-fit"
-        >
-          <div
-            :style="{ backgroundColor: color.color.code }"
-            class="shadow-[0_0_2px_0] shadow-secondary-900/50 rounded-md w-full h-6"
+      <label
+        v-for="theme in data?.items"
+        :key="theme.id"
+        class="group space-y-2 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 border border-transparent has-checked:border-primary-900 rounded-xl w-full h-fit cursor-pointer"
+      >
+        <div class="flex flex-row justify-between items-center w-full">
+          <h3 class="font-bold">{{ theme.name }}</h3>
+          <input
+            v-model="currentTheme"
+            type="radio"
+            :value="theme.id"
+            name="theme"
+            class="size-5 accent-primary-900 cursor-pointer"
           />
         </div>
-      </div>
-    </label>
+        <div class="gap-2 grid grid-cols-4 w-full">
+          <div
+            v-for="color in theme.themeColors"
+            :key="color.color.code"
+            class="w-full h-fit"
+          >
+            <div
+              :style="{ backgroundColor: color.color.code }"
+              class="shadow-[0_0_2px_0] shadow-secondary-900/50 rounded-md w-full h-6"
+            />
+          </div>
+        </div>
+      </label>
+    </div>
     <button
       @click="updateTheme"
       class="flex flex-row justify-center items-center space-x-2 bg-primary-900 hover:bg-primary-100 p-2 rounded-xl w-full h-12 text-background-900 hover:text-primary-900 transition duration-300 ease-in-out cursor-pointer"
