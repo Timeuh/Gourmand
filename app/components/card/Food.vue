952dc760ae99b70d054a97f7e93d1f26836172c3
Timeuh <timeuuh@gmail.com>
2026-08-14 21:11:15 +0200

    💄 change card black background to app secondary color

--- app/components/card/Food.vue
+++ app/components/card/Food.vue
@@ -21,10 +21,10 @@ function openEditModal() {
   <CardBase :class="props.class" :food="props.cardFood">
     <template #overlay>
       <div
-        class="bottom-0 absolute flex flex-row justify-between items-center bg-linear-to-t from-black via-60% via-black/80 to-black/20 backdrop-blur-xs p-2 w-full"
+        class="bottom-0 absolute flex flex-row justify-between items-center bg-linear-to-t from-secondary-900 via-60% via-secondary-900/80 to-secondary-900/20 backdrop-blur-xs p-2 w-full"
       >
         <div class="space-y-1 w-full">
-          <h3 class="text-background-900 text-sm text-balance">
+          <h3 class="font-bold text-secondary-100 text-sm text-balance">
             {{ props.cardFood.name }}
           </h3>
         </div>
