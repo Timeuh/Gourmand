a6f318b9263a1644fbbaea93b86dbd2769cf7da6
Timeuh <timeuuh@gmail.com>
2026-08-14 17:38:55 +0200

    ✨ get current theme to change app colors

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const { currentThemeName } = useTheme();
+</script>
+
 <template>
-  <main class="xl:flex flex-row min-w-full min-h-screen">
+  <main
+    class="xl:flex flex-row min-w-full min-h-screen"
+    :class="currentThemeName"
+  >
     <Navbar />
     <Sidebar />
     <slot />
