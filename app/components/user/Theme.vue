0ddbf01707247657d47a333a3b58c6c50cb43847
Timeuh <timeuuh@gmail.com>
2026-08-14 17:37:58 +0200

    ✨ display themes and handle different theme selection and saving for user

--- app/components/user/Theme.vue
+++ app/components/user/Theme.vue
@@ -1,34 +1,9 @@
 <script lang="ts" setup>
+// get theme utils from composable
+const { data, currentTheme, updateTheme } = useTheme();
+
 // get user session utils
-const { user, fetch, loggedIn } = useUserSession();
-
-// get toast display utils
-const { displayToast } = useToast();
-
-// currently selected theme
-const currentTheme = ref<number>(user.value?.theme_id || 1);
-
-// update user theme in database and session
-async function updateTheme() {
-  // do nothing if theme is the currently used one
-  if (currentTheme.value == user.value?.theme_id) return;
-
-  // update in database and in user session
-  await $fetch(`/api/users/${user.value?.id || 0}`, {
-    method: "PUT",
-    body: {
-      email: user.value?.email,
-      theme_id: currentTheme.value,
-      month_objective: user.value?.month_objective,
-    },
-  });
-
-  // refresh session
-  await fetch();
-
-  // display a toast to tell user the food has been added
-  displayToast("Thème modifié");
-}
+const { loggedIn } = useUserSession();
 </script>
 
 <template>
@@ -40,12 +15,40 @@ async function updateTheme() {
       <h2 class="font-bold text-2xl text-center">Thème de l'application</h2>
       <h3 class="text-md text-secondary-500">Choisissez un thème</h3>
     </div>
+    <label
+      v-for="theme in data?.items"
+      :key="theme.id"
+      class="group space-y-2 shadow-[0_0_2px_0] shadow-secondary-900/50 p-2 border border-transparent has-checked:border-primary-900 rounded-xl w-full"
+    >
+      <div class="flex flex-row justify-between items-center w-full">
+        <h3 class="font-bold">{{ theme.name }}</h3>
+        <input
+          v-model="currentTheme"
+          type="radio"
+          :value="theme.id"
+          name="theme"
+          class="size-5 accent-primary-900"
+        />
+      </div>
+      <div class="gap-2 grid grid-cols-4 w-full">
+        <div
+          v-for="color in theme.themeColors"
+          :key="color.color.code"
+          class="w-full h-fit"
+        >
+          <div
+            :style="{ backgroundColor: color.color.code }"
+            class="shadow-[0_0_2px_0] shadow-secondary-900/50 rounded-md w-full h-6"
+          />
+        </div>
+      </div>
+    </label>
     <button
       @click="updateTheme"
       class="flex flex-row justify-center items-center space-x-2 bg-primary-900 hover:bg-primary-100 p-2 rounded-xl w-full h-12 text-background-900 hover:text-primary-900 transition duration-300 ease-in-out cursor-pointer"
     >
       <IconEdit class="size-6" />
-      <h4 class="text-lg">Mettre à jour</h4>
+      <h4 class="text-lg">Sauvegarder</h4>
     </button>
   </section>
 </template>
