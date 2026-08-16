2df0a57943164341c1428e0579a1110ff6f5deb4
Timeuh <timeuuh@gmail.com>
2026-08-16 18:19:29 +0200

    🔧 replace previous env var bar currently used one

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-mkdir -p "$IMAGES_DIR/uploads"
-mkdir -p "$IMAGES_DIR/assets"
+mkdir -p "$NUXT_IPX_FS_DIR/uploads"
+mkdir -p "$NUXT_IPX_FS_DIR/assets"
 
-chown -R nuxt:nodejs "$IMAGES_DIR"
+chown -R nuxt:nodejs "$NUXT_IPX_FS_DIR"
 
 exec su -s /bin/sh nuxt -c "$*"
