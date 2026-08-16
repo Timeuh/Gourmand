bfa412b72d8f61dfe948fbb2bc8a61105381ef25
Timeuh <timeuuh@gmail.com>
2026-08-16 18:55:35 +0200

    💬 change log button text

--- app/components/button/Log.vue
+++ app/components/button/Log.vue
@@ -13,6 +13,6 @@ today.setHours(12, 0, 0, 0);
     class="flex flex-row justify-center items-center space-x-2 bg-secondary-100 hover:bg-secondary-900 shadow-[0_1px_2px_0] shadow-secondary-900/25 rounded-lg outline-none w-1/2 h-10 text-secondary-900 hover:text-secondary-100 xl:text-lg transition-color duration-300 ease-in-out cursor-pointer"
   >
     <IconPlus class="size-5 xl:size-6" />
-    <h3>Log un plat</h3>
+    <h3>Ajouter un plat</h3>
   </button>
 </template>
