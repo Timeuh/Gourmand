872ea6b76e2938e5d0c0174d676185ec46db5ac5
Timeuh <timeuuh@gmail.com>
2026-08-16 17:44:35 +0200

    ✨ create login page

---
+++ app/pages/login.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+useHead({
+  title: "Gourmand - Connexion",
+});
+</script>
+
+<template>
+  <div
+    class="space-y-6 bg-background-500 mb-20 xl:mb-0 p-8 w-full xl:w-5/6 min-h-screen select-none"
+  >
+    <section id="first-row">
+      <h1 class="font-bold text-secondary-900 text-xl">👤 Connexion</h1>
+      <h2 class="text-md text-secondary-500">
+        {{ formatDate(new Date()) }}
+      </h2>
+    </section>
+    <UserAccount />
+  </div>
+</template>
