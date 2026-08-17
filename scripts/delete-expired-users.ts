a4cac218f9daa553a50638ec7929febcb792ef8b
Timeuh <timeuuh@gmail.com>
2026-08-17 23:17:28 +0200

    🩹 fix image delete path

--- scripts/delete-expired-users.ts
+++ scripts/delete-expired-users.ts
@@ -29,7 +29,7 @@ async function main() {
     if (!food.image) continue;
 
     try {
-      await unlink("../../.data" + food.image);
+      await unlink("../../../.data" + food.image);
     } catch (error) {
       if ((error as NodeJS.ErrnoException).code !== "ENOENT") {
         throw error;
